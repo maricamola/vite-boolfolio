0a0ee9bcc3fc554edab73516404dd6fa09abcf23
maricamola <maricamola98@libero.it>
2023-06-26 18:11:29 +0200

    [First commit] README

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+
+import {store} from './store/store.js';
+import axios from 'axios';
+
+export default {
+  name:'home',
+
+  data(){
+    return{
+      projects:[]
+    }
+  },
+
+  methods:{
+    getApi(){
+
+      axios.get(store.apiUrl + 'projects')
+      .then(results => {
+        this.projects = results.data;
+        console.log(this.projects);
+      })
+    },
+
+    FormatData(dataString){
+      const d = new Date(dateString);
+      return d.toLocaleDateString()
+    }
+  },
+
+  mounted(){
+    this.getApi();
+  }
+
+
+
+}
+
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>Elenco progetti</h1>
+
+    <ul>
+      <li
+        v-for="project in projects"
+        :key="project.id"
+      >
+
+      <span>{{ project.title }}</span>
+
+      <!-- <span>{{ formatDate(project.date_creation) }}</span> -->
+
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+@use './scss/style.scss'
+
+</style>
